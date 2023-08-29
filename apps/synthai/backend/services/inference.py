287bb9540a24bae84602d789b96cc15931710c2a
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-08-29 11:13:07 +0200

    add:HF explicit api_token usage

--- apps/synthai/backend/services/inference.py
+++ apps/synthai/backend/services/inference.py
@@ -39,9 +39,10 @@ def create_llm(task: Literal["summarize"] | Literal["synthesis"]):
                                                                       request_timeout=120
                                                                       )
         else:
-            llm = HuggingFaceHub(repo_id="tiiuae/falcon-7b-instruct")
+            llm = HuggingFaceHub(repo_id="tiiuae/falcon-7b-instruct",huggingfacehub_api_token=settings.HUGGINGFACEHUB_API_TOKEN)
         return llm
     except ValueError as e:
+        print(e)
         raise ValueError(
             "The task must be either 'summarize' or 'synthesis' and the OPENAI_API_KEY or the HUGGINGFACEHUB_API_TOKEN must be set.")
 
