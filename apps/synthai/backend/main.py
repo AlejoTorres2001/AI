a782e733681c8cbb034b26131163d2960ba2d8dc
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-11-28 19:54:52 +0100

    add ES prompts

--- apps/synthai/backend/main.py
+++ apps/synthai/backend/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 
-from prompts import MAP_PROMPT_EN, COMBINE_PROMPT_EN
+from prompts import MAP_PROMPT_EN, COMBINE_PROMPT_EN, MAP_PROMPT_ES, COMBINE_PROMPT_ES
 
 from services.parsers import construct_pdf, parse_json, validate_schema, extract_text, get_documents
 from services.data import create_embeddings, get_relevant_documents_indexes, get_documents_by_index
@@ -31,32 +31,40 @@ async def upload_file(file: UploadFile = File(..., description="a document in PD
     ###! ---- Schema validations ---- ###
     try:
         parsed_json = parse_json(parameters)
+        print("parameters parsed!")
         model_parameters = validate_schema(parsed_json)
+        print("parameters validated!")
         ###! ---- Data Pipeline ---- ###
         pdf_file = await construct_pdf(file)
+        print("pdf constructed!")
         full_text = extract_text(
             pdf_file, start=model_parameters.from_page, finish=model_parameters.to_page)
-
+        print("text extracted!")
         documents = get_documents(full_text, separators=[
             "\n\n", "\n", "\t"], chunk_size=model_parameters.chunk_size, chunk_overlap=model_parameters.chunk_overlap)
-
+        print("documents created!")
         embeddings = create_embeddings(documents)
+        print("embeddings created!")
         doc_indexes = get_relevant_documents_indexes(
             embeddings, num_clusters=model_parameters.clusters_number)
         relevant_documents = get_documents_by_index(
             indexes=doc_indexes, docs=documents)
+        print("documents filtered!")
 
         ###! ---- Inference ---- ###
 
         sum_llm = create_llm(task="summarize")
-        summaries = await summarize_docs(llm=sum_llm, prompt=MAP_PROMPT_EN, input_variables=["text"], documents=relevant_documents)
+        summaries = await summarize_docs(llm=sum_llm, prompt=MAP_PROMPT_EN if model_parameters.language == "EN" else MAP_PROMPT_ES, input_variables=["text"], documents=relevant_documents)
+        print("documents summarized!")
 
         synth_llm = create_llm(task="synthesize")
-        result = await synthesis(llm=synth_llm, prompt=COMBINE_PROMPT_EN, input_variables=["text"], summary_list=summaries)
+        result = await synthesis(llm=synth_llm, prompt=COMBINE_PROMPT_EN if model_parameters.language == "EN" else COMBINE_PROMPT_ES, input_variables=["text"], summary_list=summaries)
+        print("documents combined!")
 
         ###! ---- Response ---- ###
         response_data = {"message": "File summarized correctly",
                          "summary": result}
+        print(result)
         return JSONResponse(content=jsonable_encoder(response_data))
     except Exception as e:
         return JSONResponse(content=jsonable_encoder({"message": f"{e}", "summary": "{}"}), status_code=500)
--- apps/synthai/backend/prompts.py
+++ apps/synthai/backend/prompts.py
@@ -6,6 +6,25 @@ Your response should be at least three paragraphs and fully encompass what was s
 ```{text}```
 FULL SUMMARY:
 """
+
+MAP_PROMPT_ES = """
+Se te dara una seccion de un articulo. Esta seccion estara encerrada en triple backticks (```)
+Tu objetivo es dar un resumen de esta seccion para que el lector tenga un entendimiento completo de lo que paso.
+Tu respuesta debe ser de al menos tres parrafos y abarcar completamente lo dicho en el pasaje.
+
+```{text}```
+RESUMEN COMPLETO:
+"""
+
+COMBINE_PROMPT_ES = """
+Se te dara una serie de resumenes de un articulo. Los resumenes estaran encerrados en triple backticks (```)
+Tu objetivo es dar un resumen extenso de lo que paso en la historia.
+El lector debe ser capaz de entender de que trata el articulo.
+
+```{text}```
+RESUMEN EXTENSO:
+"""
+
 COMBINE_PROMPT_EN = """
 You will be given a series of summaries from an article. The summaries will be enclosed in triple backticks (```)
 Your goal is to give a verbose summary of what happened in the story.
